53283c7a0e5d1a30a837ac938fc8bf34c5fa8260
zoeyzhao <zoeyzhao19@gmail.com>
2021-06-30 14:55:00 +0800

    build: vite config add vite-plugin-html

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import viteSvgIcons from "vite-plugin-svg-icons";
 import path from "path";
+import { minifyHtml, injectHtml } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,23 +12,30 @@ export default defineConfig({
       // 配置路劲在你的src里的svg存放文件
       iconDirs: [path.resolve(__dirname, "src/icons/svg")],
       symbolId: "icon-[dir]-[name]"
+    }),
+    minifyHtml(),
+    injectHtml({
+      injectData: {
+        title: "Vite App",
+        cdn: process.env.NODE_ENV === "development" ? false : true
+      }
     })
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
     }
-  },
-  build: {
-    // 依赖外置
-    rollupOptions: {
-      external: ["vue", "element3"],
-      output: {
-        globals: {
-          vue: "Vue",
-          element3: "ELEMENT"
-        }
-      }
-    }
   }
+  // build: {
+  //   // 依赖外置
+  //   rollupOptions: {
+  //     external: ["vue", "element3"],
+  //     output: {
+  //       globals: {
+  //         vue: "Vue",
+  //         element3: "ELEMENT"
+  //       }
+  //     }
+  //   }
+  // }
 });
